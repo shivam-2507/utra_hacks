5cd5ffe31b02dade9e5eab214349ab053c27af16
Shivam Walia <shivamwalia2006@gmail.com>
2025-02-01 13:04:25 -0500

    testing

---
+++ src/servo.cpp
@@ -0,0 +1,17 @@
+// #include <Arduino.h>
+// #include <Servo.h> // Include the Servo library
+
+// Servo myServo; // Create a servo object
+
+// void setup()
+// {
+//     myServo.attach(2); // Attach the servo to pin 9
+// }
+
+// void loop()
+// {
+//     myServo.write(0);   // Move the servo to 0 degrees
+//     delay(1000);        // Wait for 1 second
+//     myServo.write(180); // Move the servo to 0 degrees
+//     delay(1000);        // Wait for 1 second
+// }
