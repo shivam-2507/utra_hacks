56e51531ffcf9ac45bad7e886bbd06d78c253923
kartikbudihal <kpbudihal@gmail.com>
2025-02-01 12:15:38 -0500

    added functions file:

---
+++ src/functions.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <string>
+#include <iostream>
+using namespace std;
+
+string getColor(vector<int> color) {
+
+    string finalColor;
+
+    vector<int> redVector = {255, 0, 0};
+    vector<int> blueVector = {0, 255, 0};
+    vector<int> blueVector = {0, 0, 255};
+
+    return finalColor;
+}
